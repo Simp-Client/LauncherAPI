7e0220ddc20fa40eadbd9b44a09c8a978eb1dda8
Simp-Client <80881502+Simp-Client@users.noreply.github.com>
2021-03-18 10:51:11 +0200

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -2,15 +2,15 @@ function whitelistCheck() {
   document.getElementById('launchButton').innerHTML = "Checking WL";
   fs.readFile(getAppDataPath('.minecraft/launcher_accounts.json'), function (err, data) {
     if (err) throw err;
-    if(data.indexOf('5802512617c24fe3bbc981519341f7d6') >= 1) {
+    if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
       launchBeta()
-    } else if(data.indexOf('26bc0d301adf4d1a9742da5e0cd9922a') >= 1) {
+    } else if(data.indexOf('83c987216c66438ebf3c2e1d0dd6d469') >= 1) {
       launchBeta()
-    } else if(data.indexOf('076be689a2a44354b46b78f8b313b589') >= 1) {
+    } else if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
       launchBeta()
-    } else if(data.indexOf('d90df151096649228d0ca58e1fc5f3fb') >= 1) {
+    } else if(data.indexOf('1a16f19cd20c48c1b36106789f7adfb0') >= 1) {
       launchBeta()
-    } else if(data.indexOf('dd64c67732c4459f877e4c71244b7061') >= 1) {
+    } else if(data.indexOf('1a16f19cd20c48c1b36106789f7adfb0') >= 1) {
       launchBeta()
     } else {
       notWhitelisted()
